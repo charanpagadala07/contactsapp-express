65167bf78a61b252d17a8dc617a2276a9cf76b2a
charanpagadala07 <charanpagadala2004@gmail.com>
2025-02-21 00:01:51 +0530

    second commit

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
+const erroHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 
 const app = express();
 
 const port = process.env.PORT || 5000;
-
-app.get('/api/contacts', (req, res)=>{
-    res.send("getting all contacts")
-})
+app.use(express.json());
+app.use("/api/contacts", require("./routes/ContactRoutes"));
+app.use(erroHandler);
 
 app.listen(port, ()=>{
     console.log(`server running on ${port}`);
